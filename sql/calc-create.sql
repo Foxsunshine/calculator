d24798e9ffa38114c68a1fffd39915eebdb246eb
Jiang Cheng <kaihatsu.chien.jan@gmail.com>
2023-07-26 15:57:34 +0900

    fixedSql

--- sql/calc-create.sql
+++ sql/calc-create.sql
@@ -2,6 +2,18 @@ CREATE DATABASE IF NOT EXISTS `aiful`;
 USE `aiful`;
 
 --
+-- Table structure for table `equation_log`
+--
+DROP TABLE IF EXISTS `equation_log`;
+
+CREATE TABLE `equation_log` (
+  `id` int NOT NULL,
+  `equation` varchar(255) DEFAULT NULL,
+  `result` varchar(45) DEFAULT NULL,
+  `summit_date` DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+--
 -- Table structure for table `personal_information`
 --
 DROP TABLE IF EXISTS `personal_information`;
@@ -16,14 +28,3 @@ CREATE TABLE `personal_information` (
   FOREIGN KEY (`equation_id`) REFERENCES `equation_log` (`id`)
 );
 
---
--- Table structure for table `equation_log`
---
-DROP TABLE IF EXISTS `equation_log`;
-
-CREATE TABLE `equation_log` (
-  `id` int NOT NULL,
-  `equation` varchar(255) DEFAULT NULL,
-  `result` varchar(45) DEFAULT NULL,
-  `summit_date` DATETIME DEFAULT CURRENT_TIMESTAMP
-);
