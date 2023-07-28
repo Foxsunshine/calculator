ed82dee6789afe71f6c8619e917bb5b5978ad28d
Jiang Cheng <kaihatsu.chien.jan@gmail.com>
2023-07-28 11:29:19 +0900

    sqlUpdate

--- sql/calc-create.sql
+++ sql/calc-create.sql
@@ -2,20 +2,6 @@ CREATE DATABASE IF NOT EXISTS `aiful`;
 USE `aiful`;
 
 --
--- Table structure for table `equation_log`
---
-DROP TABLE IF EXISTS `equation_log`;
-
-CREATE TABLE `equation_log` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `equation` VARCHAR(255) DEFAULT NULL,
-  `result` VARCHAR(45) DEFAULT NULL,
-  `summit_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
-  `status` VARCHAR(3) DEFAULT '00',
-  PRIMARY KEY (`id`)
-);
-
---
 -- Table structure for table `personal_information`
 --
 DROP TABLE IF EXISTS `personal_information`;
@@ -24,9 +10,21 @@ CREATE TABLE `personal_information` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) DEFAULT NULL,
   `email` VARCHAR(45) DEFAULT NULL,
-  `phone_number` VARCHAR(45) DEFAULT NULL,
-  `equation_id` INT DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `idx_equation_id` (`equation_id`), -- Add an index for the foreign key column
-  FOREIGN KEY (`equation_id`) REFERENCES `equation_log` (`id`) -- Reference the `id` column in `equation_log`
+  PRIMARY KEY (`id`)
 );
+
+--
+-- Table structure for table `equation_log`
+--
+DROP TABLE IF EXISTS `equation_log`;
+
+CREATE TABLE `equation_log` (
+  `personal_id` INT DEFAULT NULL,
+  `equation` VARCHAR(255) DEFAULT NULL,
+  `result` VARCHAR(45) DEFAULT NULL,
+  `summit_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  INDEX `idx_personal_id` (`personal_id`), -- Add an index for the foreign key column
+  FOREIGN KEY (`personal_id`) REFERENCES `personal_information` (`id`) -- Reference the `id` column in `equation_log`
+);
+
+
--- sql/calc-data.sql
+++ sql/calc-data.sql
@@ -3,16 +3,8 @@
 --
 
 INSERT INTO
-  `equation_log` (id)
+  `personal_information` (name, email)
 VALUES
-('001'),
-  ('002'),
-  ('003');
-
-
-INSERT INTO
-  `personal_information` (name, email,phone_number,equation_id)
-VALUES
-('松井　孝典', 'matsuii@gmail.com','00000001','001'),
-  ('翁長　靖武', 'onaga@gmail.com','00000002','002'),
-  ('ジャン　チェン', 'jangcheng@gmail.com','00000003','003');
+('松井　孝典', 'matsuii@gmail.com'),
+  ('翁長　靖武', 'onaga@gmail.com'),
+  ('ジャン　チェン', 'jangcheng@gmail.com');
